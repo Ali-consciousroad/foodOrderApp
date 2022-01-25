fd729da45eaa8f334238be71f479da235a6b9725
Ali-consciousroad <ali_dindar@live.be>
2022-01-25 16:00:27 +0100

    Create a store with CartContext and CartProvider to manage the states of our components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-  
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
@@ -16,14 +16,14 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {/* Make the cart dissapear because initialy, cartIsShown value is false */}
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
